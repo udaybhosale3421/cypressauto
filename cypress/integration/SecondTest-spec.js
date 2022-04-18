0f4549ac689349f627099c365fe710960a24bb86
Udaysingh Bhosale <bhosale.udaysingh3421@gmail.com>
2022-04-18 15:08:17 +0530

    Second Test added

---
+++ cypress/integration/SecondTest-spec.js
@@ -0,0 +1,13 @@
+//<reference type="cypress"/>
+describe("UI Elements1",function()
+    {
+        it("Verify types of locators1",function()
+        {
+            cy.visit("https://demo.nopcommerce.com/");
+            cy.get('#small-searchterms').type('Apple MacBook Pro 13-inch');
+            cy.get('button[type=submit]').click();
+            cy.title().should('eq','nopCommerce demo store. Search');
+        })
+    }
+
+)
