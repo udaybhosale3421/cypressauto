63fa94d4c0479f9558392f51e18a0facf5b5b45a
Udaysingh Bhosale <bhosale.udaysingh3421@gmail.com>
2022-04-18 13:05:37 +0530

    Adding code to firsttest

---
+++ cypress/integration/FirstTest-spec.js
@@ -0,0 +1,14 @@
+
+//<reference type="cypress"/>
+describe("UI Elements",function()
+    {
+        it("Verify types of locators",function()
+        {
+            cy.visit("https://demo.nopcommerce.com/");
+            cy.get('#small-searchterms').type('Apple MacBook Pro 13-inch');
+            cy.get('button[type=submit]').click();
+            cy.title().should('eq','nopCommerce demo store. Search');
+        })
+    }
+
+)
