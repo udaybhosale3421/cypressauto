93f296f197ac74ea6a576d738267e6066ebc90de
Udaysingh Bhosale <bhosale.udaysingh3421@gmail.com>
2022-05-05 10:45:30 +0530

    first test updated

--- cypress/integration/FirstTest.js
+++ cypress/integration/FirstTest.js
@@ -1,6 +1,10 @@
 describe('My First Test Case',function(){
-    it('does not match',function(){
-        expect(true).to.equal(false);
-    }
+    it("Verify types of locators1",function()
+    {
+        cy.visit("https://demo.nopcommerce.com/");
+        cy.get('#small-searchterms').type('Apple MacBook Pro 13-inch');
+        cy.get('button[type=submit]').click();
+        cy.title().should('eq','nopCommerce demo store. Search');
+    })
     )
 })
